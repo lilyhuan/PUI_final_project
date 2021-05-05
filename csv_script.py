7f831703a126e5c68053b3b29f975ec0262dab48
lilyhuan <61841737+lilyhuan@users.noreply.github.com>
2021-05-04 20:36:27 -0500

    author1 added, book2 added

--- csv_script.py
+++ csv_script.py
@@ -10,16 +10,33 @@ def remove_country_name(s):
 
 def separate_books(s):
     splitted = s.split('/')
-    print(splitted)
+    # print(splitted)
+    if len(splitted) < 3:
+        return 'None'
+    else:
+        return splitted[2][1:-1]
+    # print(splitted[1][:-1])
+    # print(repr(s))
+
+def separate_author(s):
+    #first space
+    splitted = s.split(' ')
+    if len(splitted) < 2:
+        return s
+    else:
+        author = [splitted[0], splitted[1]]
+        return ' '.join(author)
 
 with open(path, 'r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
         books = row[1]
+        book = row[2]
         # print(books)
         # print(remove_country_name(books))
-        separate_books(books)
+        # print(separate_books(books))
+        print(separate_author(book))
         line_count += 1
 
     print(line_count)
