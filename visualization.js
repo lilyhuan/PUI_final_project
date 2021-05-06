541a85d3f82ec03e0c904f9b56a2d657f7697324
lilyhuan <61841737+lilyhuan@users.noreply.github.com>
2021-05-05 21:00:46 -0500

    Google Books API up, cards up

--- visualization.js
+++ visualization.js
@@ -5,8 +5,11 @@
 
     var svg = d3.select("#map")
     .append("svg")
+    // .attr("height", height + margin.top + margin.bottom)
+    // .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .attr("width", width + margin.left + margin.right)
+    .attr("id", "map")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
@@ -28,22 +31,22 @@
     .projection(projection) //make shape from dots
 
     function ready (error, data) {
-        console.log(data)
+        // console.log(data)
         var countries = topojson.feature(data, data.objects.countries).features
 
-        console.log(countries)
+        // console.log(countries)
 
         svg.selectAll(".country")
         .data(countries)
         .enter().append("path")
         .attr("class", "country")
         .attr("d", path)
-        .on('mouseover', function(d) {
+        .on('click', function(d) {
             d3.select(this).classed("selected", true)
             var child = this
             var parent = child.parentNode;
             var index = Array.prototype.indexOf.call(parent.children, child);
-            console.log(countries[index].properties.name)
+            // console.log(countries[index].properties.name)
             changeBook(countries[index].properties.name)
         })
         .on('mouseout', function(d) {
@@ -53,8 +56,95 @@
 }) ();
 
 
-function changeBook(country) {
+function changeBook(cname) {
     let card = document.getElementById("book")
-    card.innerHTML = country
+    card.innerHTML = cname
 
+    d3.csv("/books.csv", function (data) {
+        for (var i = 0; i < data.length; i++) {
+        //   document.getElementById("book").innerHTML = data[i].Country;
+        //   console.log(data[i].Country);
+        //   console.log(data[i].Book);
+        if (data[i].Country == cname) {
+            // card.innerHTML += data[i].Book1
+            let books = data[i]
+            // APICall(books.Title1, books.Author1)
+            if (books.Title3 != "None") {
+                APICall(books.Title3, books.Author3)
+            }
+            if (books.Title2 != "None") {
+                APICall(books.Title2, books.Author2)
+            }
+            if (books.Title1 != "None") {
+                APICall(books.Title1, books.Author1)
+            }
+
+        }
+        }
+      });
 }
+
+// AIzaSyCwOZ_Mai04gnxvoiEJTj0A6cQ9zft1LOs
+// GET https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=yourAPIKey
+
+function APICall(title, author) {
+    // const userAction = async () => {
+    //     const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyCwOZ_Mai04gnxvoiEJTj0A6cQ9zft1LOs');
+    //     const myJson = await response.json(); //extract JSON from the http response
+    //     // do something with myJson
+    //     console.log(myJson)
+    //   }
+    const request = new XMLHttpRequest();
+    console.log(`https://www.googleapis.com/books/v1/volumes?q=${title}+inauthor:${author}&key=AIzaSyCwOZ_Mai04gnxvoiEJTj0A6cQ9zft1LOs`)
+    request.open("GET", `https://www.googleapis.com/books/v1/volumes?q=${title}+inauthor:${author}&key=AIzaSyCwOZ_Mai04gnxvoiEJTj0A6cQ9zft1LOs`)
+    request.send();
+    request.onload = () => {
+        if(request.status === 200) {
+            console.log(request.response)
+            // console.log(request.response.items)
+            const obj = JSON.parse(request.response)
+            // console.log(obj.items[0].volumeInfo.imageLinks)
+            if (obj.totalItems != 0) {
+                let card = document.getElementById("book")
+            card.innerHTML += `
+            <div class="card mb-3" style="max-width: 540px;">
+              <div class="row g-0">
+                <div class="col-md-4">
+                  <img src="${obj.items[0].volumeInfo.imageLinks.thumbnail}" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">${title}</h5>
+                    <p class="card-text">${author}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            `
+            } else {
+                console.log("BOOK NOT FOUND")
+            }
+            
+        } else {
+            console.log("failed")
+        }
+    }
+}
+
+
+var card = `
+<div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="..." alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+`
