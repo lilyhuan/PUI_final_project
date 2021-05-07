d8e8c2a165b9fa3ec2384509a4c662200a53b18d
lilyhuan <61841737+lilyhuan@users.noreply.github.com>
2021-05-07 13:57:30 -0500

    moved files to assets, removed commented out code

--- assets/visualization.js
+++ assets/visualization.js
@@ -1,56 +1,38 @@
 // https://www.youtube.com/watch?v=aNbgrqRuoiE
 
 (function() {
-    var margin = { top: 50, left: 50, right: 50, bottom: 50 },
-    height = 400 - margin.top - margin.bottom,
-    width = 800 - margin.left - margin.right;
+    height = 400
     width = document.getElementById('map-container').offsetWidth
     var mtop = 75
+
     if (window.outerWidth < 480) {
       document.getElementById('top-map-margin').className = "row mt-2"
-      mtop = 20
+      mtop = 0
     }
-    // height = document.getElementById('map-container').offsetHeight
-    console.log(width, height)
     var svg = d3.select("#map-container")
     .append("svg")
-    // .attr("height", height + margin.top + margin.bottom)
-    // .attr("width", width + margin.left + margin.right)
     .attr("height", 400)
     .attr("width", "100%")
-    // .attr("class", "img-fluid")
     .attr("id", "map")
     .append("g")
     .attr("transform", "translate(" + 0 + "," + mtop + ")");
-    // .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
     
     d3.queue()
-    .defer(d3.json, "world.topojson")
+    .defer(d3.json, "assets/world.topojson")
     .await(ready)
-    // d3.json("world.topojson", function(data) {
-    //     console.log(data);
-    //     console.log("hi")
-    // });
 
     // Mercator projection
     var projection = d3.geoMercator()
     .translate([ width/2, height/2 ]) //center it
-    // .scale(90) //zoom in out
     .scale(width / 2.5 / Math.PI)
-    //             .rotate([0, 0])
-    //             .center([0, 0])
-                // .translate([width / 2, height / 2])
+
 
     // create path using projection
     var path = d3.geoPath()
     .projection(projection) //make shape from dots
 
     function ready (error, data) {
-        // console.log(data)
         var countries = topojson.feature(data, data.objects.countries).features
-
-        // console.log(countries)
-
         svg.selectAll(".country")
         .data(countries)
         .enter().append("path")
@@ -61,7 +43,6 @@
             var child = this
             var parent = child.parentNode;
             var index = Array.prototype.indexOf.call(parent.children, child);
-            // console.log(countries[index].properties.name)
             changeBook(countries[index].properties.name)
         })
         .on('mouseout', function(d) {
@@ -72,45 +53,30 @@
 
 
 function changeBook(cname) {
-    // let card = document.getElementById("book")
-    // card.innerHTML = cname
     document.getElementById("country-name").innerText = cname
 
-    d3.csv("https://lilyhuan.github.io/PUI_final_project/books.csv", function (data) {
-      // d3.csv("/books.csv", function (data) {
+    // d3.csv("https://lilyhuan.github.io/PUI_final_project/assets/books.csv", function (data) {
+      d3.csv("assets/books.csv", function (data) {
         for (var i = 0; i < data.length; i++) {
-        //   document.getElementById("book").innerHTML = data[i].Country;
-        //   console.log(data[i].Country);
-        //   console.log(data[i].Book);
-        if (data[i].Country == cname) {
-            // card.innerHTML += data[i].Book1
-            let books = data[i]
-            // APICall(books.Title1, books.Author1)
-            if (books.Title3 != "None") {
-                APICall(books.Title3, books.Author3)
-            }
-            if (books.Title2 != "None") {
-                APICall(books.Title2, books.Author2)
-            }
-            if (books.Title1 != "None") {
-                APICall(books.Title1, books.Author1)
-            }
+          if (data[i].Country == cname) {
+              let books = data[i]
+              if (books.Title3 != "None") {
+                  APICall(books.Title3, books.Author3)
+              }
+              if (books.Title2 != "None") {
+                  APICall(books.Title2, books.Author2)
+              }
+              if (books.Title1 != "None") {
+                  APICall(books.Title1, books.Author1)
+              }
 
-        }
+          }
         }
       });
 }
 
-// AIzaSyCwOZ_Mai04gnxvoiEJTj0A6cQ9zft1LOs
-// GET https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=yourAPIKey
-
+// API key: AIzaSyCwOZ_Mai04gnxvoiEJTj0A6cQ9zft1LOs
 function APICall(title, author) {
-    // const userAction = async () => {
-    //     const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyCwOZ_Mai04gnxvoiEJTj0A6cQ9zft1LOs');
-    //     const myJson = await response.json(); //extract JSON from the http response
-    //     // do something with myJson
-    //     console.log(myJson)
-    //   }
     const request = new XMLHttpRequest();
     console.log(`https://www.googleapis.com/books/v1/volumes?q=${title}+inauthor:${author}&key=AIzaSyCwOZ_Mai04gnxvoiEJTj0A6cQ9zft1LOs`)
     request.open("GET", `https://www.googleapis.com/books/v1/volumes?q=${title}+inauthor:${author}&key=AIzaSyCwOZ_Mai04gnxvoiEJTj0A6cQ9zft1LOs`)
@@ -118,9 +84,7 @@ function APICall(title, author) {
     request.onload = () => {
         if(request.status === 200) {
             console.log(request.response)
-            // console.log(request.response.items)
             const obj = JSON.parse(request.response)
-            // console.log(obj.items[0].volumeInfo.imageLinks)
             if (obj.totalItems != 0) {
                 let card = document.getElementById("book")
             card.innerHTML += `
